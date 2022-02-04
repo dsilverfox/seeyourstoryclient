36696a331ed33a4b396282071593e8d4a1be6bbb
Devon Silverfox-Patchett <DE.Silverfox.Coding@gmail.com>
2022-02-03 19:51:49 -0500

    Added more to admin logic

--- src/components/ProtectedRoutes/Admin/AdminLogic.tsx
+++ src/components/ProtectedRoutes/Admin/AdminLogic.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
 
+interface AdminVars {
+    users: object
+}
 
 
-class AdminLogic extends React.Component{
+class AdminLogic extends React.Component <{}, AdminVars> {
     constructor(props){
         super(props)
-        
+        this.state = {
+        }
     }
-
+    
+    viewUsers = async (event: React.SyntheticEvent) => {
+        event.preventDefault();
+        await fetch("http://localhost:3000/auth/userinfo", {
+            method: "GET",
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            }),
+        })
+        .then((res)=> res.json())
+        .then((users) => {
+            this.setState({users:{users}});
+        })
+    }
 
     render() {
         return(
-            <div>
-
+            <div className="Buttons">
+                <button onClick={(event) => {this.viewUsers}}>View Users</button>
             </div>
         )
     }
