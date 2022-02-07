3fe17c4fb5af118af293b39ea4a23578d75bd889
Devon Silverfox-Patchett <DE.Silverfox.Coding@gmail.com>
2022-02-06 19:01:13 -0500

    added carousel to Example Display for displaying tutorial

--- src/components/UnprotectedRoutes/Example/ExampleDisplay.tsx
+++ src/components/UnprotectedRoutes/Example/ExampleDisplay.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React from "react";
+import { Carousel } from "react-bootstrap";
 
 const ExampleDisplay = () => {
     return (
-        <div>
-            I AM THE ExampleDisplay!
-            I will hold the example user that users can walk through if they haven't registered or logged in to see what the app does.
-        </div>
+        <Carousel className='carousel'>
+            <Carousel.Item>
+                <img
+                    alt='Mango'
+                    className="d-block w-100"
+                    height='500px'
+                    width='auto'
+
+                    src='https://images.unsplash.com/photo-1623930376395-0f3ad22cfac2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80'
+
+                />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    alt='Almond'
+                    className="d-block w-100"
+                    height="500px"
+                    width="auto"
+                    src='https://images.unsplash.com/photo-1596812179749-82d8af17b7e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+
+                />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    alt="lime"
+                    className="d-block w-100"
+                    height="500px"
+                    width='auto'
+                    src='https://images.unsplash.com/photo-1564637604374-af8bcb3e9a98?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+
+                />
+            </Carousel.Item>
+        </Carousel>
     )
 }
-
 export default ExampleDisplay;
