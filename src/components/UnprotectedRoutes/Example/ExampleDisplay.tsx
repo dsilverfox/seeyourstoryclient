2a1213bdf420288a9a8450c33f27a5781cb745ed
Devon Silverfox-Patchett <DE.Silverfox.Coding@gmail.com>
2022-02-06 19:12:32 -0500

    Added controlled carousel, slides are not in carousel mode. They appear like I just stacked images.

--- src/components/UnprotectedRoutes/Example/ExampleDisplay.tsx
+++ src/components/UnprotectedRoutes/Example/ExampleDisplay.tsx
@@ -3,37 +3,41 @@ import { Carousel } from "react-bootstrap";
 
 const ExampleDisplay = () => {
     return (
-        <Carousel className='carousel'>
+        <Carousel>
             <Carousel.Item>
                 <img
-                    alt='Mango'
                     className="d-block w-100"
-                    height='500px'
-                    width='auto'
-
-                    src='https://images.unsplash.com/photo-1623930376395-0f3ad22cfac2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80'
-
+                    src="holder.js/800x400?text=First slide&bg=373940"
+                    alt="First slide"
                 />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
-                    alt='Almond'
                     className="d-block w-100"
-                    height="500px"
-                    width="auto"
-                    src='https://images.unsplash.com/photo-1596812179749-82d8af17b7e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
-
+                    src="holder.js/800x400?text=Second slide&bg=282c34"
+                    alt="Second slide"
                 />
+
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
-                    alt="lime"
                     className="d-block w-100"
-                    height="500px"
-                    width='auto'
-                    src='https://images.unsplash.com/photo-1564637604374-af8bcb3e9a98?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
-
+                    src="holder.js/800x400?text=Third slide&bg=20232a"
+                    alt="Third slide"
                 />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
     )
