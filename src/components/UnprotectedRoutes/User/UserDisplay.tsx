1bd0398a7cb4008eaaf8abd3d94fa4320cc350ef
Devon Silverfox-Patchett <DE.Silverfox.Coding@gmail.com>
2022-02-12 12:48:00 -0500

    commenting out user display to focus on AppBar display of username and ID.

--- src/components/UnprotectedRoutes/User/UserDisplay.tsx
+++ src/components/UnprotectedRoutes/User/UserDisplay.tsx
@@ -1,31 +1,33 @@
-import React from 'react';
-import { Card, Button } from 'react-bootstrap'
+// import React from 'react';
+// import { Card, Button } from 'react-bootstrap'
 
-interface UserDisplayProps {
-    username: string,
-    userId: string
-}
+// interface UserDisplayProps {
+//     username: string,
+//     userId: string
+// }
 
-class UsersDisplay extends React.Component<UserDisplayProps, {}> {
+// class UsersDisplay extends React.Component<UserDisplayProps, {}> {
 
-    render(): React.ReactNode {
-        console.log("UserName at User Display", this.props.username)
-        console.log("UserID at User Display", this.props.userId)
-        return (
-            <div>
-                <Card style={{ width: '10rem' }}>
-                    {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-                    <Card.Body>
-                        <Card.Title>Welcome {this.props.username}</Card.Title>
-                        <Card.Text>
-                            Your user ID is {this.props.userId}
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-            </div>
-        )
-    }
-}
+//     render(): React.ReactNode {
+//         console.log("UserName at User Display", this.props.username)
+//         console.log("UserID at User Display", this.props.userId)
+//         return (
+//             <div>
+//                 <Card style={{ width: '10rem' }}>
+//                     {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+//                     <Card.Body>
+//                         <Card.Title>Welcome {this.props.username}</Card.Title>
+//                         <Card.Text>
+//                             Your user ID is {this.props.userId}
+//                         </Card.Text>
+//                         <Button variant="primary">Go somewhere</Button>
+//                     </Card.Body>
+//                 </Card>
+//             </div>
+//         )
+//     }
+// }
 
-export default UsersDisplay;
+// export default UsersDisplay;
+
+export {};
