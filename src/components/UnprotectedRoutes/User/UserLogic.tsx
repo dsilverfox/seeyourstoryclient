987fe4496182aa87387ba068b425bfbafcc2ff5a
Devon Silverfox-Patchett <DE.Silverfox.Coding@gmail.com>
2022-02-12 16:07:59 -0500

    Verified register works, added data.user console to check why we're getting reading id on login

--- src/components/UnprotectedRoutes/User/UserLogic.tsx
+++ src/components/UnprotectedRoutes/User/UserLogic.tsx
@@ -123,10 +123,11 @@ class UserLogic extends React.Component<userProps, UserVars> {
             }),
         }).then((response) => response.json())
             .then((data) => {
-                console.log("DATA FOR SETSTATE TOKEN", data)
-                console.log("Data for UserId", data.user.id)
-                console.log("Data for Username", data.user.username)
-                console.log("Data for hasAdmin?", data.user.hasAdmin)
+                // console.log("DATA FOR SETSTATE TOKEN", data)
+                // console.log("Data for UserId", data.user.id)
+                // console.log("Data for Username", data.user.username)
+                // console.log("Data for hasAdmin?", data.user.hasAdmin)
+                console.log(data.user)
                 this.props.setAdmin(data.user.hasAdmin)
                 this.props.updateToken(data.sessionToken)
                 this.props.setUsername(data.user.username)
